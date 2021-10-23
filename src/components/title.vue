ba6522c5a076bae5f1290b4d76a4d2710f4515e4
ユーザー名 <yusaku_nakamura_aa@icloud.com>
2021-10-23 17:34:21 +0900

    login,detail

---
+++ src/components/title.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+
+<script>
+export default{
+    name: 'title',
+    data: {
+        title: "課題１を終わらせる"
+    },
+    watch: {
+        title: function() {
+            localStorage.setItem('title', JSON.stringify(this.title));
+        },
+    },
+    mounted: function(){
+        this.title = JSON.parse(localStorage.getItem('title'))  || [];
+    }
+}
+</script>
