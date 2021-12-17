e540fb71f4eb3cf575b24730180d61a27f7f4403
akaishikiso <lycopene109@outlook.jp>
2021-12-17 19:57:46 +0900

    新規タスク作成ボタン作った

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,55 @@
 <template>
   <v-app>
-    <div id="app">
-    <!-- <div
-      id="nav"
-      class="tab-area-base">
-      <ul class="tab-menu-base">
-        <li><router-link to="/">Login</router-link></li>
-        <li><router-link to="/list">Todo List</router-link></li>
-        <li><router-link to="/detail">Todo Detail</router-link></li>
-      </ul>
-    </div> -->
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
       <router-view/>
-    </div>
+    </v-main>
   </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-#nav {
-  text-align: center;
-}
-.tab-menu-base {
-  margin: 0;
-  padding: 0;
-}
-#nav ul li {
-  list-style: none;
-  display: inline-block;
-  width: 18%;
-  min-width: 90px;
-}
-</style>
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/pages/TodoShowPage.vue
+++ src/pages/TodoShowPage.vue
@@ -2,40 +2,40 @@
   <div class="detail">
     <!--ここにhtmlを書いてね-->
     <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>ToDo</title>
-    </head>
-    <body>
+      </head>
+      <body>
         <header>
-            <img src="#">
+          <img src="#" />
         </header>
         <div class="title" id="goal">
-            タスク：<input type="text" style="border:none" v-model="title">
+          タスク：<input type="text" style="border:none" v-model="title" />
         </div>
         <div class="goalDate" id="app">
-            <p class="caption">終わらせたい日時</p>
-            <input type="datetime-local" style="border:none" v-model="date1">
+          <p class="caption">終わらせたい日時</p>
+          <input type="datetime-local" style="border:none" v-model="date1" />
         </div>
         <div class="limitDate">
-            <p class="caption">締め切り日時</p>
-            <input type="datetime-local" style="border:none" v-model="date2">
+          <p class="caption">締め切り日時</p>
+          <input type="datetime-local" style="border:none" v-model="date2" />
         </div>
         <div class="notification">
-            <p class="caption">通知</p>
-            <input type="datetime-local" style="border:none" v-model="date3">
+          <p class="caption">通知</p>
+          <input type="datetime-local" style="border:none" v-model="date3" />
         </div>
         <div class="memo">メモ</div>
         <div class="form" id="note">
-            <textarea cols="47" style="border:none" v-model="memo"></textarea>
+          <textarea cols="47" style="border:none" v-model="memo"></textarea>
         </div>
         <div class="finish">
-            <a href="#">Done!</a>
+          <a href="#">Done!</a>
         </div>
 
         <a href="#" @click="deleteItem(index)" class="try">戻る</a>
-    </body>
+      </body>
     </html>
   </div>
 </template>
@@ -43,172 +43,173 @@
 <script>
 // ここにjavascriptを書いてね
 export default {
-    name: 'TodoShowPage',
-    data(){
-        return{
-            todo: {},
-        }
-    },
-    // watch: {
-    //     title: function() {
-    //         localStorage.setItem('title', JSON.stringify(this.title));
-    //     },
-    //     date1: function() {
-    //         localStorage.setItem('date1', JSON.stringify(this.date1));
-    //     },
-    //     date2: function() {
-    //         localStorage.setItem('date2', JSON.stringify(this.date2));
-    //     },
-    //     date3: function() {
-    //         localStorage.setItem('date3', JSON.stringify(this.date3));
-    //     },
-    //     memo: function() {
-    //         localStorage.setItem('memo', JSON.stringify(this.memo));
-    //     },
-    // },
-    // mounted: function(){
-    //     this.title = JSON.parse(localStorage.getItem('title'))  || [];
-    //     this.date1 = JSON.parse(localStorage.getItem('date1'))  || [];
-    //     this.date2 = JSON.parse(localStorage.getItem('date2'))  || [];
-    //     this.date3 = JSON.parse(localStorage.getItem('date3'))  || [];
-    //     this.memo = JSON.parse(localStorage.getItem('memo'))  || [];
-    // },
+  name: 'TodoShowPage',
+  data() {
+    return {
+      todo: {},
+    };
+  },
+  // watch: {
+  //     title: function() {
+  //         localStorage.setItem('title', JSON.stringify(this.title));
+  //     },
+  //     date1: function() {
+  //         localStorage.setItem('date1', JSON.stringify(this.date1));
+  //     },
+  //     date2: function() {
+  //         localStorage.setItem('date2', JSON.stringify(this.date2));
+  //     },
+  //     date3: function() {
+  //         localStorage.setItem('date3', JSON.stringify(this.date3));
+  //     },
+  //     memo: function() {
+  //         localStorage.setItem('memo', JSON.stringify(this.memo));
+  //     },
+  // },
+  // mounted: function(){
+  //     this.title = JSON.parse(localStorage.getItem('title'))  || [];
+  //     this.date1 = JSON.parse(localStorage.getItem('date1'))  || [];
+  //     this.date2 = JSON.parse(localStorage.getItem('date2'))  || [];
+  //     this.date3 = JSON.parse(localStorage.getItem('date3'))  || [];
+  //     this.memo = JSON.parse(localStorage.getItem('memo'))  || [];
+  // },
 
-    // by ほりしょー
-    // mounted は最初のレンダリングで呼ばれる。
-    // 将来的にはここでAPI(Django)を呼び出してデータを取得する。
-    // 現状は sampleTodos をAPIと見立てて todo に代入してあげている。
-    mounted() {
-        const sampleTodos = [
-            {
-              title: '課題１を終わらせる',
-              goalDate: '2021-8-17.19：00',
-              limitDate: '2021-8-20.23：59',
-              notification: '2021-8-16.9：00',
-              memo: 'なし',
-              rest: '2',
-            },
-            {
-              title: 'レポートAを終わらせる',
-              goalDate: '2021-8-20.21:00',
-              limitDate: '2021-8-26.23:00',
-              notification: '8-19.9:00',
-              memo: '4000字だよ一日じゃ終わらないよ',
-              rest: '5',
-            },
-            {
-              title: '経営学テキスト読んでくる',
-              goalDate: '2021-9-1.23:00',
-              limitDate: '2021-9-2.10:30',
-              notification: '2021-8-31.9:00',
-              memo: 'p.123-150',
-              rest: '16',
-            },
-            {
-              title: '機構アカウント作る',
-              goalDate: '2021-9-20.21:00',
-              limitDate: '2021-9-27.23:59',
-              notofication: '2021-9-19.9:00',
-              memo: 'なし',
-              rest: '36',
-            },
-        ]
-        // this.$route.params で URL のパラメータ部分を取得できる。
-        this.todo = sampleTodos[this.$route.params.id]
+  // by ほりしょー
+  // mounted は最初のレンダリングで呼ばれる。
+  // 将来的にはここでAPI(Django)を呼び出してデータを取得する。
+  // 現状は sampleTodos をAPIと見立てて todo に代入してあげている。
+  mounted() {
+    const sampleTodos = [
+      {
+        title: '課題１を終わらせる',
+        goalDate: '2021-8-17.19：00',
+        limitDate: '2021-8-20.23：59',
+        notification: '2021-8-16.9：00',
+        memo: 'なし',
+        rest: '2',
+      },
+      {
+        title: 'レポートAを終わらせる',
+        goalDate: '2021-8-20.21:00',
+        limitDate: '2021-8-26.23:00',
+        notification: '8-19.9:00',
+        memo: '4000字だよ一日じゃ終わらないよ',
+        rest: '5',
+      },
+      {
+        title: '経営学テキスト読んでくる',
+        goalDate: '2021-9-1.23:00',
+        limitDate: '2021-9-2.10:30',
+        notification: '2021-8-31.9:00',
+        memo: 'p.123-150',
+        rest: '16',
+      },
+      {
+        title: '機構アカウント作る',
+        goalDate: '2021-9-20.21:00',
+        limitDate: '2021-9-27.23:59',
+        notofication: '2021-9-19.9:00',
+        memo: 'なし',
+        rest: '36',
+      },
+    ];
+    // this.$route.params で URL のパラメータ部分を取得できる。
+    this.todo = sampleTodos[this.$route.params.id];
+  },
+  methods: {
+    deleteItem: function(index) {
+      //モーダルを閉じる
+      // リストからカードを削除
+      this.schedules.splice(index, 1);
     },
-    methods: {
-        deleteItem: function(index){
-            //モーダルを閉じる
-            // リストからカードを削除
-            this.schedules.splice(index, 1);
-        }
-    },
-}
+  },
+};
 </script>
 
 <style scoped>
 /* ここにcssを書いてね */
 header {
-    height: 50px;
-    background-color:  #ea5532;
+  height: 50px;
+  background-color: #ea5532;
 }
 
 img {
-    height: 40px;
-    padding: 5px 0 0 10px;
+  height: 40px;
+  padding: 5px 0 0 10px;
 }
 
-.title{
-    font-size: 20px;
-    padding-top: 15px;
-    padding-bottom: 15px;
-    padding-left: 10px;
-    color: rgb(143, 143, 143);
+.title {
+  font-size: 20px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  padding-left: 10px;
+  color: rgb(143, 143, 143);
 }
 
-.goalDate, .limitDate, .notification {
-    display: flex;
-    border-top: 1px solid #ea5532;
-    justify-content: space-between;
-    color: rgb(143, 143, 143);
+.goalDate,
+.limitDate,
+.notification {
+  display: flex;
+  border-top: 1px solid #ea5532;
+  justify-content: space-between;
+  color: rgb(143, 143, 143);
 }
 
-.caption{
-    padding-left: 10px;
+.caption {
+  padding-left: 10px;
 }
 
-.memo{
-    border-top: 1px solid #ea5532;
-    padding-top: 16px;
-    padding-left: 10px;
-    color: rgb(143, 143, 143);
+.memo {
+  border-top: 1px solid #ea5532;
+  padding-top: 16px;
+  padding-left: 10px;
+  color: rgb(143, 143, 143);
 }
 
 .form {
-    border-bottom: 1px solid #ea5532;
-    padding-top: 16px;
-    padding-bottom: 16px;
-    padding-left: 10px;
+  border-bottom: 1px solid #ea5532;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  padding-left: 10px;
 }
 
-
 .finish {
-    background-color: #ea5532;
-    width: 150px;
-    height: 40px;
-    border-radius: 8px;
-    line-height: 40px;
-    text-align: center;
-    margin:0 auto;
-    margin-top: 16px;
-    box-shadow: 1px 1px #b3381c;
+  background-color: #ea5532;
+  width: 150px;
+  height: 40px;
+  border-radius: 8px;
+  line-height: 40px;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 16px;
+  box-shadow: 1px 1px #b3381c;
 }
 
 .finish:active {
-    box-shadow: none;
-    position: relative;
-    top: 1px;
-    left: 1px;
+  box-shadow: none;
+  position: relative;
+  top: 1px;
+  left: 1px;
 }
 
 a {
-    text-decoration: none;
-    color: white;
-    display: block;
+  text-decoration: none;
+  color: white;
+  display: block;
 }
 
-input[type="datetime-local"] {
-    height: 30px;
-    margin-top: 12px;
-    margin-right: 10px;
-    width: 185px;
+input[type='datetime-local'] {
+  height: 30px;
+  margin-top: 12px;
+  margin-right: 10px;
+  width: 185px;
 }
 
-input[type="text"] {
-    font-size: 20px;
+input[type='text'] {
+  font-size: 20px;
 }
 
 .try {
-    color: #b3381c;
+  color: #b3381c;
 }
 </style>
