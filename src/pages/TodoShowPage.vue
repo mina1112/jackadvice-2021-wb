e0582b211f1e2129a612bb7dc61e7df846816deb
yutteee <yusaku_nakamura_aa@icloud.com>
2022-02-19 17:46:03 +0900

    コンフリクトが正しく消せてなかったので書き直した

--- src/pages/TodoShowPage.vue
+++ src/pages/TodoShowPage.vue
@@ -77,93 +77,93 @@ export default {
       const sampleTodos = [
         {
           title: '課題1を終わらせる',
-          goalDate: '2021-8-17.19:00',
-          limitDate: '2021-8-20.23:59',
-          notification: '2021-8-16.9:00',
+          goalDate: '2021-08-17T19:00',
+          limitDate: '2021-08-20T23:59',
+          notification: '2021-08-16T09:00',
           memo: 'なし',
           rest: '2',
         },
         {
           title: 'レポートAを終わらせる',
-          goalDate: '2021-8-20.21:00',
-          limitDate: '2021-8-26.23:00',
-          notification: '8-19.9:00',
+          goalDate: '2021-08-20T21:00',
+          limitDate: '2021-08-26T23:00',
+          notification: '2021-08-19T09:00',
           memo: '4000字だよ一日じゃ終わらないよ',
           rest: '5',
         },
         {
           title: '経営学テキスト読んでくる',
-          goalDate: '2021-9-1.23:00',
-          limitDate: '2021-9-2.10:30',
-          notification: '2021-8-31.9:00',
+          goalDate: '2021-09-01T23:00',
+          limitDate: '2021-09-02T10:30',
+          notification: '2021-08-31T09:00',
           memo: 'p.123-150',
           rest: '16',
         },
         {
           title: '機構アカウント作る',
-          goalDate: '2021-9-20.21:00',
-          limitDate: '2021-9-27.23:59',
-          notofication: '2021-9-19.9:00',
+          goalDate: '2021-09-20T21:00',
+          limitDate: '2021-09-27T23:59',
+          notofication: '2021-09-19T09:00',
           memo: 'なし',
           rest: '36',
         },
       ];
       // this.$route.params で URL のパラメータ部分を取得できる。
-      this.todo = sampleTodos[this.$route.params.id];
+      this.todo = sampleTodos[this.$route.params.id-1];
     },
-    methods: {
-      deleteItem: function(index) {
-        //モーダルを閉じる
-        // リストからカードを削除
-        this.schedules.splice(index, 1);
-      },
+    // methods: {
+    //   deleteItem: function(index) {
+    //     //モーダルを閉じる
+    //     // リストからカードを削除
+    //     this.schedules.splice(index, 1);
+    //   },
       // by ほりしょー
       // mounted は最初のレンダリングで呼ばれる。
       // 将来的にはここでAPI(Django)を呼び出してデータを取得する。
       // 現状は sampleTodos をAPIと見立てて todo に代入してあげている。
-      mounted() {
-          const sampleTodos = [
-                  {
-                    id: 1,
-                    title: '課題１を終わらせる',
-                    goalDate: '2021-08-17T19:00',
-                    limitDate: '2021-08-20T23:59',
-                    notification: '2021-08-16T09:00',
-                    memo: 'なし',
-                    rest: '2',
-                  },
-                  {
-                    id: 2,
-                    title: 'レポートAを終わらせる',
-                    goalDate: '2021-08-20T21:00',
-                    limitDate: '2021-08-26T23:00',
-                    notification: '2021-08-19T09:00',
-                    memo: '4000字だよ一日じゃ終わらないよ',
-                    rest: '5',
-                  },
-                  {
-                    id: 3,
-                    title: '経営学テキスト読んでくる',
-                    goalDate: '2021-09-01T23:00',
-                    limitDate: '2021-09-02T10:30',
-                    notification: '2021-08-31T09:00',
-                    memo: 'p.123-150',
-                    rest: '16',
-                  },
-                  {
-                    id: 4,
-                    title: '機構アカウント作る',
-                    goalDate: '2021-09-20T21:00',
-                    limitDate: '2021-09-27T23:59',
-                    notification: '2021-09-19T09:00',
-                    memo: 'なし',
-                    rest: '36',
-                  },
-              ]
-          // this.$route.params で URL のパラメータ部分を取得できる。
-          this.todo = sampleTodos[this.$route.params.id-1]
-      },
-    }
+      // mounted() {
+      //     const sampleTodos = [
+      //             {
+      //               id: 1,
+      //               title: '課題１を終わらせる',
+      //               goalDate: '2021-08-17T19:00',
+      //               limitDate: '2021-08-20T23:59',
+      //               notification: '2021-08-16T09:00',
+      //               memo: 'なし',
+      //               rest: '2',
+      //             },
+      //             {
+      //               id: 2,
+      //               title: 'レポートAを終わらせる',
+      //               goalDate: '2021-08-20T21:00',
+      //               limitDate: '2021-08-26T23:00',
+      //               notification: '2021-08-19T09:00',
+      //               memo: '4000字だよ一日じゃ終わらないよ',
+      //               rest: '5',
+      //             },
+      //             {
+      //               id: 3,
+      //               title: '経営学テキスト読んでくる',
+      //               goalDate: '2021-09-01T23:00',
+      //               limitDate: '2021-09-02T10:30',
+      //               notification: '2021-08-31T09:00',
+      //               memo: 'p.123-150',
+      //               rest: '16',
+      //             },
+      //             {
+      //               id: 4,
+      //               title: '機構アカウント作る',
+      //               goalDate: '2021-09-20T21:00',
+      //               limitDate: '2021-09-27T23:59',
+      //               notification: '2021-09-19T09:00',
+      //               memo: 'なし',
+      //               rest: '36',
+      //             },
+      //         ]
+      //     // this.$route.params で URL のパラメータ部分を取得できる。
+      //     this.todo = sampleTodos[this.$route.params.id-1]
+      // },
+    // }
 }
 </script>
 
@@ -172,11 +172,6 @@ export default {
 .detail{
     text-align: left;
     font-size: 16px!important;
-}
-
-header {
-  height: 50px;
-  background-color: #ea5532;
 }
 
 img {
@@ -234,7 +229,7 @@ input[type='datetime-local'] {
   height: 30px;
   margin-top: 12px;
   margin-right: 10px;
-  width: 185px;
+  width: 200px;
   position: relative;
   bottom: 16px;
 }
