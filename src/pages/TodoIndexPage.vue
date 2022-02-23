28f75e84a1e26916b0ab28acffc13a076e4aae46
Riko YAMADA <lycopene109@outlook.jp>
2022-02-23 15:30:00 +0900

    サンプルを用いてaxiosでデータを取得できるようにした。　

--- src/pages/TodoIndexPage.vue
+++ src/pages/TodoIndexPage.vue
@@ -11,10 +11,10 @@
           <Header />
         </header>
         <div class="main">
-          <div v-for="todo in sampleTodos" :key="todo.title">
+          <!-- <div v-for="todo in sampleTodos" :key="todo.id">
             <div class="card">
               <div class="todo">
-                <!-- ここが遷移の部分。参考：https://router.vuejs.org/ja/api/#to -->
+                ここが遷移の部分。参考：https://router.vuejs.org/ja/api/#to
                 <router-link
                   :to="{ name: 'TodoShowPage', params: { id: todo.id } }"
                   class="title"
@@ -24,19 +24,32 @@
                 <div class="rest">あと{{ todo.rest }}日</div>
               </div>
               <a @click="end" class="finish">Done!</a>
-              <!--以下モーダル-->
-              <!--
+              <a href="" class="finish">Done!</a>
+              以下モーダル
                 v-bindしてpostItemを指定することで、Work.vueで定義されたデータをModal.vueに送ることができる。
                 Modal.vue側でvalueと書くとpostItemのことになる。
                 ここで、postItemはopenModal(item)の引数itemを代入してあるので、結果としてvalueでitemを受け取れる。
-               -->
               <modal
                 :value="postItem"
                 v-show="showContent"
                 @close="closeModal"
               />
+            </div> -->
+          <ul v-for="todo in todo" :key="todo.id">
+            <div class="card">
+              <div class="todo">
+                <router-link
+                  :to="{ name: 'TodoShowPage', params: { id: todo.id } }"
+                  class="title"
+                  >{{ todo.title }}</router-link
+                >
+                <div class="goalDate">{{ todo.completed }}</div>
+                <div class="rest">あと{{ todo.id }}日</div>
+              </div>
+              <a href="" class="finish">Done!</a>
             </div>
-          </div>
+          </ul>
+          <!-- </div> -->
 
           <div>
             <router-link
@@ -53,94 +66,108 @@
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
+import Header from '@/components/Header.vue';
+import axios from 'axios';
 
 export default {
   name: 'TodoIndexPage',
   components: {
-    Header
+    Header,
   },
   data() {
     return {
-      showContent: false,
-      postItem: '',
-      items: [
-        {
-          id: 1,
-          desc: 'aaaaa',
-          range: 'iiiii',
-        },
-        {
-          id: 1,
-          desc: 'uuuuu',
-          range: 'eeeee',
-        },
-      ],
-      sampleTodos: [
-        {
-          id: 1,
-          title: '課題１を終わらせる',
-          goalDate: '2021-8-17.19：00',
-          limitDate: '2021-8-20.23：59',
-          notification: '2021-8-16.9：00',
-          memo: 'なし',
-          rest: '2',
-        },
-        {
-          id: 2,
-          title: 'レポートAを終わらせる',
-          goalDate: '2021-8-20.21:00',
-          limitDate: '2021-8-26.23:00',
-          notification: '8-19.9:00',
-          memo: '4000字だよ一日じゃ終わらないよ',
-          rest: '5',
-        },
-        {
-          id: 3,
-          title: '経営学テキスト読んでくる',
-          goalDate: '2021-9-1.23:00',
-          limitDate: '2021-9-2.10:30',
-          notification: '2021-8-31.9:00',
-          memo: 'p.123-150',
-          rest: '16',
-        },
-        {
-          id: 4,
-          title: '機構アカウント作る',
-          goalDate: '2021-9-20.21:00',
-          limitDate: '2021-9-27.23:59',
-          notofication: '2021-9-19.9:00',
-          memo: 'なし',
-          rest: '36',
-        },
-      ],
+      todo: null,
+      // showContent: false,
+      // postItem: '',
+      // items: [
+      //   {
+      //     id: 1,
+      //     desc: 'aaaaa',
+      //     range: 'iiiii',
+      //   },
+      //   {
+      //     id: 1,
+      //     desc: 'uuuuu',
+      //     range: 'eeeee',
+      //   },
+      // ],
+      //   todos: [
+      //     {
+      //       id: 1,
+      //       title: '課題１を終わらせる',
+      //       goalDate: '2021-8-17.19：00',
+      //       limitDate: '2021-8-20.23：59',
+      //       notification: '2021-8-16.9：00',
+      //       memo: 'なし',
+      //       rest: '2',
+      //     },
+      //     {
+      //       id: 2,
+      //       title: 'レポートAを終わらせる',
+      //       goalDate: '2021-8-20.21:00',
+      //       limitDate: '2021-8-26.23:00',
+      //       notification: '8-19.9:00',
+      //       memo: '4000字だよ一日じゃ終わらないよ',
+      //       rest: '5',
+      //     },
+      //     {
+      //       id: 3,
+      //       title: '経営学テキスト読んでくる',
+      //       goalDate: '2021-9-1.23:00',
+      //       limitDate: '2021-9-2.10:30',
+      //       notification: '2021-8-31.9:00',
+      //       memo: 'p.123-150',
+      //       rest: '16',
+      //     },
+      //     {
+      //       id: 4,
+      //       title: '機構アカウント作る',
+      //       goalDate: '2021-9-20.21:00',
+      //       limitDate: '2021-9-27.23:59',
+      //       notofication: '2021-9-19.9:00',
+      //       memo: 'なし',
+      //       rest: '36',
+      //     },
+      //   ],
     };
   },
 
-  //画像がクリックされた時に実行されるopenModalメソッド
-  methods: {
-    end: function () {
-      // const openModal = (item) => {
-      //   //showContentというモーダルが表示されているかどうかのステートを管理する変数をtrueに変更
-      //   this.showContent = true;
-
-      //   //Modal.vueにデータを受け渡しする為の変数postItemに、itemを代入
-      //   this.postItem = item;
-      // };
-      const res = confirm('button was clicked.');
-      if (res == true) {
-        alert('おめでとうございます！');
-      } else {
-        alert('キャンセルされました');
-      }
-    },
-    register: function () {},
+  mounted() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => (this.todo = response.data))
+      .catch((error) => console.log(error));
   },
+
+  // method: {
+  //   end() {
+  //     console.log('hogehoge');
+  //   },
+  // },
+
+  //画像がクリックされた時に実行されるopenModalメソッド
+  // methods: {
+  //   end: function () {
+  //     const openModal = (item) => {
+  //       //showContentというモーダルが表示されているかどうかのステートを管理する変数をtrueに変更
+  //       this.showContent = true;
+
+  //       //Modal.vueにデータを受け渡しする為の変数postItemに、itemを代入
+  //       this.postItem = item;
+  //     };
+  //     const res = confirm('button was clicked.');
+  //     if (res == true) {
+  //       alert('おめでとうございます！');
+  //     } else {
+  //       alert('キャンセルされました');
+  //     }
+  //   },
+  //   register: function () {},
+  // },
 };
 </script>
 
 <style scoped>
-
 img {
   height: 40px;
   padding: 5px 0 0 10px;
