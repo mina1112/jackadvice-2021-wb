7ec7b849bcd2a8222262219dbd38541321984637
riku-81 <riku20020801@gmail.com>
2022-03-18 20:30:31 +0900

    api作れたような気がします

--- fastapi/controllers.py
+++ fastapi/controllers.py
@@ -1,12 +1,12 @@
 from ast import For
 from fastapi import FastAPI, Depends, HTTPException, Form 
-from fastapi.security import HTTPBasic, HTTPBasicCredentials  # new
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
-from starlette.status import HTTP_401_UNAUTHORIZED  # new
-from  ..frontend.src import pages
-import db  # new
+from starlette.status import HTTP_401_UNAUTHORIZED 
+from fastapi.responses import JSONResponse
+import db 
 from models import User, Task 
 from auth import auth
 from datetime import datetime
@@ -17,39 +17,22 @@ security = HTTPBasic()
 def index(request: Request):
     return {'Hello': 'World'}
 
-def admin(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
-    username = auth(credentials)
-    # Basic認証で受け取った情報
-    user = db.session.query(User).filter(User.username == username).first()
-    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+def admin(request: Request):
+
+    task = db.session.query(Task).all()
     db.session.close()
+    return JSONResponse(content = task)
 
-    return pages.TemplateResponse('TodoShowPage.vue',
-                                    {'request': request,
-                                    'task': task})
 
-def detail(request: Request, username, taskid, credentials: HTTPBasicCredentials = Depends(security)):
-    username_tmp = auth(credentials)
-    
-    if username_tmp != username:
-        return RedirectResponse("/")
-    
-    user = db.session.query(User).filter(User.username == username).first()
-    task = db.session.query(Task).filter(Task.id == taskid).all()
+
+def detail(request: Request, t_id):
+
+    task = db.session.query(Task).filter(Task.id == t_id).first()
     db.session.close()
-    return pages.TemplateResponse('Todoindex.vue',
-                                        {'request': request,
-                                        'task': task,
-                                        })
+    return JSONResponse(content = task)
 
-async def add(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
-    # 認証
-    username = auth(credentials)
+async def add(request: Request):
 
-    # ユーザ情報を取得
-    user = db.session.query(User).filter(User.username == username).first()
-
-    # フォームからデータを取得
     data = await request.form()
     year = int(data['year'])
     month = int(data['month'])
@@ -61,34 +44,25 @@ async def add(request: Request, credentials: HTTPBasicCredentials = Depends(secu
                         hour=hour, minute=minute)
 
     # 新しくタスクを生成しコミット
-    task = Task(user.id, data['content'], deadline)
+    task = Task(data['content'], deadline)
     db.session.add(task)
     db.session.commit()
     db.session.close()
 
     return RedirectResponse('/admin')
 
-def delete(request: Request, t_id, credentials: HTTPBasicCredentials = Depends(security)):
-    # 認証
-    username = auth(credentials)
+def delete(request: Request, t_id):
 
-    # ログインユーザ情報を取得
-    user = db.session.query(User).filter(User.username == username).first()
-
-    # 該当タスクを取得
     task = db.session.query(Task).filter(Task.id == t_id).first()
 
-    # もしユーザIDが異なれば削除せずリダイレクト
-    if task.user_id != user.id:
-        return RedirectResponse('/admin')
-
-    # 削除してコミット
     db.session.delete(task)
     db.session.commit()
     db.session.close()
 
     return RedirectResponse('/admin')
 
+
+"""
 def get(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
     # 認証
     username = auth(credentials)
@@ -111,14 +85,12 @@ def get(request: Request, credentials: HTTPBasicCredentials = Depends(security))
         'done': t.done,
     } for t in task]
 
-    return task
+    return JSONResponse(task)
 
 async def insert(request: Request, taskname: str = Form(...), 
                 content: str = Form(...), deadline: str = Form(...), date: str = Form(...), 
                 credentials: HTTPBasicCredentials = Depends(security)):
-    """
-    タスクを追加してJSONで新規タスクを返す。「deadline」は%Y-%m-%d_%H:%M:%S (e.g. 2019-11-03_12:30:00)の形式
-    """
+    
     # 認証
     username = auth(credentials)
 
@@ -144,3 +116,4 @@ async def insert(request: Request, taskname: str = Form(...),
         'date': task.date.strftime('%Y-%m-%d %H:%M:%S'),
         'done': task.done,
     }
+"""
